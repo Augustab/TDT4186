3c377d77e49281f87b13e531c94b9beb5f43890b
bjorafla <baflagen@hotmail.com>
2021-03-20 16:42:47 +0100

    Færdii med D

--- pe4/wish.c
+++ pe4/wish.c
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <ctype.h>
+
 
 #define MAX_INPUT_LEN 256 // Maximum input length + 2 (2 because of ending "\n" and \0)
 
@@ -18,11 +20,12 @@ int main() {
         return 1;
     }
     char cwd[PATH_MAX];
-    getcwd(cwd, sizeof(cwd));
+    //getcwd(cwd, sizeof(cwd));
     char delims[] = " \t\n"; // Tokens split by space or tab character
     int pid;
 
     while(1) { // TODO: find an exit condition for the loop
+        getcwd(cwd, sizeof(cwd));   // Updating CWD
         printf("\033[1;31mwish:<%s>$ \033[0m", cwd);
         fgets(input, MAX_INPUT_LEN, stdin); // fgets returns 0 on error TODO: error handling
 
@@ -68,57 +71,81 @@ int main() {
 
 
         //Execution
-
-        pid = fork();
-        if (pid > 0) //Parent process          
-        {
-            printf("Created a child Process %d\n", pid);
-            wait(NULL);
+        
+        //printf("%s\n", command);
+        //printf("Params: %s\n", params[0]);
+        
+        // Task D - Internal Commands
+        if (strcmp(command, "cd")==0  || strcmp(command, "CD")==0) {
+            if (chdir(params[0])!=0) {
+                printf("Error CD-ing, %d\n", errno);
+            }               
         }
-        else if (pid == 0) //Child process
+        else if (strcmp(command, "exit")==0 || strcmp(command, "EXIT")==0)
         {
-            char *argv[i+2];
-            argv[0] = command;
-            for(int c=0; c<i; c++) {
-                argv[c+1] = params[c];
+            printf("Exiting...\n");
+            exit(EXIT_SUCCESS);
+        }
+        
+        
+        else 
+        {
+            pid = fork();
+            if (pid > 0) //Parent process
+            {
+                printf("Created a child Process %d\n", pid);
+                wait(NULL);
             }
-            argv[i+1] = NULL;
+            else if (pid == 0) //Child process
+            {
+                char *argv[i + 2];
+                argv[0] = command;
+                for (int c = 0; c < i; c++)
+                {
+                    argv[c + 1] = params[c];
+                }
+                argv[i + 1] = NULL;
 
-            
-            if (in_file != NULL) {
-                int input = open(in_file, O_RDONLY);
-                if (input == -1) {
-                    printf("Error: InputFile can not be read or doesn't exist\n");
+                if (in_file != NULL)
+                {
+                    int input = open(in_file, O_RDONLY);
+                    if (input == -1)
+                    {
+                        printf("Error: InputFile can not be read or doesn't exist\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    else
+                    {
+                        //printf("HER ER INPUT\n");
+                        dup2(input, 0);
+                        close(input);
+                    }
+                }
+
+                if (out_file != NULL)
+                {
+                    //printf("INNE HER\n");
+                    int output = open(out_file, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR); // FLAGS FOUND HERE: http://www.cs.loyola.edu/~jglenn/702/S2005/Examples/dup2.html
+                    printf("ERROR: %d", errno);
+                    if (output == -1)
+                    {
+                        printf("ERROR: Outfile cannot be opened - Showing Result in shell instead\n");
+                        close(output);
+                    }
+                    else
+                    {
+                        //printf("HER ER OUTPUT\n");
+                        dup2(output, 1);
+                        close(output);
+                    }
+                }
+
+                if (execvp(command, argv) < 0)
+                {
+                    printf("An error has occured. Error type: %d\n", errno);
+                    //Exit failure
                     exit(EXIT_FAILURE);
                 }
-                else {
-                    //printf("HER ER INPUT\n");
-                    dup2(input, 0);
-                    close(input);
-                }
-            }
-            
-
-            if (out_file != NULL) {
-                //printf("INNE HER\n");
-                int output = open(out_file, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR); // FLAGS FOUND HERE: http://www.cs.loyola.edu/~jglenn/702/S2005/Examples/dup2.html
-                printf("ERROR: %d", errno);
-                if (output == -1) {
-                    printf("ERROR: Outfile cannot be opened - Showing Result in shell instead\n");
-                    close(output);
-                }
-                else {
-                    //printf("HER ER OUTPUT\n");
-                    dup2(output, 1);
-                    close(output);
-                }
-            }
-
-
-            if (execvp(command, argv) < 0) {
-                printf("An error has occured. Error type: %d\n", errno);
-            //Exit failure    
-            exit(EXIT_FAILURE);
             }
         }
     }
